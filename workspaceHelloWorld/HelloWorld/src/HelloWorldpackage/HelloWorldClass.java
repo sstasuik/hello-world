4ea9149c6aad531ac0ee73cc3c89692c17d6677e
sstasuik <68166185+sstasuik@users.noreply.github.com>
2020-07-19 11:43:07 -0400

    Add files via upload

---
+++ workspaceHelloWorld/HelloWorld/src/HelloWorldpackage/HelloWorldClass.java
@@ -0,0 +1,12 @@
+package HelloWorldpackage;
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
